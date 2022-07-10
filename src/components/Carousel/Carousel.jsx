a02760ff4e24f1775a5fa6e6776ef0ec343c2e88
jrbreeden <jrbreeden55@gmail.com>
2022-07-10 19:41:37 -0400

    Add Arrows to Carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,31 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Carousel.css"
-
 import { images } from '../../utilities/helpers/CarouselData'
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos"
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos"
 
 export default function Carousel() {
-  return (
-  <div className="carousel">
-    <div className="carouselInner"></div>
-  </div>
-  )
 
- //   <div className="mt-24 flex justify-center">
-   //   <div className='bg-gray-400 w-1/2 py-8 px-4 rounded'>
-     //   <Slider {...settings}>
-       //   <div>
-         //   <img className="h-96 w-full m-auto" src={images[0]} alt="" />
-//         </div>
- //         <div>
- //           <img className="h-96 m-auto" src={images[1]} alt="" />
-   //       </div>
-     //     <div>
-      //      <img className="h-96 m-auto" src={images[2]} alt="" />
-       //   </div>
-//        </Slider>
-//      </div>
- //   </div>
+  const [currentImage, setCurrentImage] = useState(0)
+
+  return (
+    <div className="carousel">
+      <div className="carouselInner"
+        style={{ backgroundImage: `url(${images[currentImage].img})` }}
+      >
+        <div className="left"
+          onClick={() => {
+            currentImage > 0 && setCurrentImage(currentImage - 1)
+          }}
+        >
+          <ArrowBackIosIcon style={{ fontsize: 30 }} />
+        </div>
+        <div className="center">
+          <h1>{images[currentImage].title}</h1>
+          <p>{images[currentImage].subtitle}</p>
+        </div>
+        <div className="right"
+          onClick={() => {
+            currentImage < images.length - 1 && setCurrentImage(currentImage + 1)
+          }}
+        >
+          <ArrowForwardIosIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
+
+
 
 //export default Carousel
 
