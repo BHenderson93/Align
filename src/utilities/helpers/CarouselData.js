3564d815fefb27b5b6447e4eccda4b8a77039c7d
jrbreeden <jrbreeden55@gmail.com>
2022-07-10 21:20:04 -0400

    Carousel works

--- src/utilities/helpers/CarouselData.js
+++ src/utilities/helpers/CarouselData.js
@@ -2,10 +2,11 @@ import myResume from "../../assets/images/myResume.jpg"
 import Computer from "../../assets/images/Computer.jpg"
 import forHire from "../../assets/images/forHire.jpg"
 export const images = [
-    { title: "About us", 
-    subtitle: "Are you tired of searching for jobs? We have an agent who can search for jobs for you", 
-    img: myResume 
-},
+    {
+        title: "About us",
+        subtitle: "Are you tired of searching for jobs? We have an agent who can search for jobs for you",
+        img: myResume
+    },
     {
         title: "Our Offering",
         subtitle: "We offer job assistance and resume help based on your skillset",
